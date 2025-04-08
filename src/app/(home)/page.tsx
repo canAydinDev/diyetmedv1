0c9d8c1327f33797dfdab056b8d9e9b8de3efe0c
canaydindev <canaydindev@gmail.com>
2025-04-08 22:44:10 +0300

    metadta

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Separator } from "@/components/ui/separator";
 import CihazMotionPage from "@/models/home/cihazlar-paketler/CihazMotionPage";
 import DidYouKnowPage from "@/models/home/did-you-know/didYouKnowPage";
@@ -6,9 +7,23 @@ import { HomeNavbar } from "@/models/home/home-navbar/HomeNavbar";
 import KampanyaPage from "@/models/home/kampanya/kampanyaPage";
 import NasilmiPage from "@/models/home/nasil-mi/nasilmiPage";
 
+// ✅ SEO: Metadata objesi
+export const metadata: Metadata = {
+  title:
+    "Uzman Diyetisyen | Mersin | Kaçırılmayacak Yaza Hazırlık Çekilişi! | Vacu Activ & Roll Shape",
+  description:
+    "Diyetmed ile Vacu Activ ve Roll Shape cihazlarıyla yaza hazırlan! Çekilişe katıl, sağlıklı dönüşüm yolculuğunu başlat!",
+};
+
 const Page = () => {
   return (
     <main className="scroll-smooth">
+      {/* ✅ SEO: sr-only H1 etiketi */}
+      <h1 className="sr-only">
+        Uzman Diyetisyen | Mersin | Kaçırılmayacak Yaza Hazırlık Çekilişi! |
+        Vacu Activ & Roll Shape
+      </h1>
+
       <HomeNavbar />
       <Welcome />
       <KampanyaPage />
@@ -16,7 +31,6 @@ const Page = () => {
       <NasilmiPage />
       <Separator />
       <CihazMotionPage />
-
       <DidYouKnowPage />
       <Separator />
     </main>
